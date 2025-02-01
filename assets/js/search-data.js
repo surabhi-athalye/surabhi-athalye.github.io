3718dec07412a2f90f76bfa05c9b472cfa96bdf9
surabhi-athalye <sathalye.website@gmail.com>
2025-02-01 17:51:28 +0000

    Deploying to gh-pages from @ surabhi-athalye/surabhi-athalye.github.io@860a4fbd7d80286439ee98f53d03cc9a18001a3f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -382,16 +382,16 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "news-american-control-conference-2025-denver-co-i-will-be-presenting-our-paper-synthesis-of-interacting-model-based-and-model-free-controllers-for-optimal-control-in-july",
-          title: 'American Control Conference 2025, Denver, CO I will be presenting our paper “Synthesis...',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+    },{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-american-control-conference-2025-denver-co-i-will-be-presenting-our-paper-synthesis-of-interacting-model-based-and-model-free-controllers-for-optimal-control-in-july",
+          title: 'American Control Conference 2025, Denver, CO I will be presenting our paper “Synthesis...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
